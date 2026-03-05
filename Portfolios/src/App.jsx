c1844a7ccc2da4dad0aea41867f577b9deb6d51f
ajah <ajahajang00@gmail.com>
2026-03-05 17:47:03 +0200

    Initial Commit:Setup

--- Portfolios/src/App.jsx
+++ Portfolios/src/App.jsx
@@ -2,13 +2,19 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { Route,Routes } from 'react-router'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      
+      <div>
+        <h1>hello</h1>
+        <Routes> 
+<Route path='/' element={<h1>Heellow 2</h1>}/>
+        </Routes>
+       </div>
     </>
   )
 }
